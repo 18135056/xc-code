cc08743c5a9026844a2669747f9a02e975e79826
wb-wangxiangchang <18135056@qq.com>
2013-05-02 14:01:19 +0800

    初始化

---
+++ xc-code-btrace/src/main/java/cn/xc/btrace/App.java
@@ -0,0 +1,13 @@
+package cn.xc.btrace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
